f70f2a2b0196d4881ae907b623b9fe045fa46ba7
emmark01 <emmarkbuenaflor0301@gmail.com>
2026-08-18 01:49:54 -0300

    Wire React Router pages into the Northwood app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { TaskProvider } from "./context/TaskContext";
+import { BoardPage } from "./pages/Board";
+import { CalendarPage } from "./pages/Calendar";
+import { DashboardPage } from "./pages/Dashboard";
+import { LoginPage } from "./pages/Login";
+import { NotFoundPage } from "./pages/NotFound";
+import { ProjectsPage } from "./pages/Projects";
+import { SettingsPage } from "./pages/Settings";
+import { SignupPage } from "./pages/Signup";
+import { TasksPage } from "./pages/Tasks";
+import { TeamPage } from "./pages/Team";
+
 export default function App() {
   return (
-    <main className="app-boot">
-      <p>Northwood is starting up.</p>
-    </main>
+    <TaskProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/board" element={<BoardPage />} />
+          <Route path="/tasks" element={<TasksPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+          <Route path="/team" element={<TeamPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </TaskProvider>
   );
 }
